6f0f56d44e0385f02997d6efda1ad25b0704b90a
Prathamesh Raut. <prathamesh1687@gmail.com>
2025-06-05 08:40:15 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,44 @@
+package Demo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Employee {
+	@Id
+	@GeneratedValue(strategy =GenerationType.IDENTITY)
+	@Column
+	private int ID;
+	
+	@Column
+	private String name;
+	
+	@Column
+	private String City;
+	
+	
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return City;
+	}
+	public void setCity(String city) {
+		City = city;
+	}	
+	
+}
